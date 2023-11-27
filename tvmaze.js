5903817c6e76e9251878dd38f5509856851c65e4
Nicholas L <NLoo1@outlook.com>
2023-11-27 16:33:07 -0500

    Completed searchForShowAndDisplay(); working on getEpisodesOfShow()

--- tvmaze.js
+++ tvmaze.js
@@ -12,27 +12,33 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-        "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ];
+  const search = await axios.get('http://api.tvmaze.com/search/shows', {params:
+  {
+    q: term
+  }})
+  console.log(search.data);
+  return search.data;
+  // return [
+  //   {
+  //     id: 1767,
+  //     name: "The Bletchley Circle",
+  //     summary:
+  //       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+  //          women with extraordinary skills that helped to end World War II.</p>
+  //        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+  //          normal lives, modestly setting aside the part they played in
+  //          producing crucial intelligence, which helped the Allies to victory
+  //          and shortened the war. When Susan discovers a hidden code behind an
+  //          unsolved murder she is met by skepticism from the police. She
+  //          quickly realises she can only begin to crack the murders and bring
+  //          the culprit to justice with her former friends.</p>`,
+  //     image:
+  //       "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  //   }
+  // ];
 }
 
 
@@ -41,17 +47,25 @@ async function getShowsByTerm( /* term */) {
 function populateShows(shows) {
   $showsList.empty();
 
+  if(shows.length <= 0){
+    // console.log("AAAA");
+    $showsList.html("No results found.");
+  }
+
   for (let show of shows) {
+
+    let imageUrl = show.show.image && show.show.image.original ? show.show.image.original : 'https://tinyurl.com/tv-missing';
+
     const $show = $(
-      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src="${imageUrl}"
+              alt="${show.show.name}"
               class="w-25 me-3">
            <div class="media-body">
-             <h5 class="text-primary">${show.name}</h5>
-             <div><small>${show.summary}</small></div>
+             <h5 class="text-primary">${show.show.name}</h5>
+             <div><small>${show.show.summary}</small></div>
              <button class="btn btn-outline-light btn-sm Show-getEpisodes">
                Episodes
              </button>
@@ -80,6 +94,16 @@ async function searchForShowAndDisplay() {
 $searchForm.on("submit", async function (evt) {
   evt.preventDefault();
   await searchForShowAndDisplay();
+
+  const $episodeButtons = $(".Show-getEpisodes");
+  // console.log($episodeButtons);
+  // console.log("AAAAAA");
+
+  $episodeButtons.on("click", async function(e){
+    e.preventDefault();
+    // console.log(this.parentElement.parentElement.parentElement.getAttribute("data-show-id"));
+    getEpisodesOfShow(this.parentElement.parentElement.parentElement.getAttribute("data-show-id"))
+  })
 });
 
 
@@ -87,8 +111,18 @@ $searchForm.on("submit", async function (evt) {
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id) {
+  console.log(id);
+  const test = document.querySelector(`div[data-show-id="${id}"]`);
+  console.log(test);
+  
+  test.append();
+}
 
 /** Write a clear docstring for this function... */
 
-// function populateEpisodes(episodes) { }
+function populateEpisodes(episodes) { 
+
+}
+
+
